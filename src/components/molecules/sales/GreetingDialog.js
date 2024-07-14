6f5c9332cd1d8b6b6aa2d89274ec3ce3d6efd49b
Mochamad Akbar <mochakbar912@gmail.com>
2024-07-14 19:17:10 +0700

    fix: add sales customer

--- src/components/molecules/sales/GreetingDialog.js
+++ src/components/molecules/sales/GreetingDialog.js
@@ -9,13 +9,47 @@ import {
   TextField,
   Grid, Box, Typography,
 } from '@mui/material';
+import Image from "next/image";
 
 const GreetingDialog = ({ open, handleClose, name }) => {
   return (
     <Dialog open={open} onClose={handleClose} fullWidth>
-      <DialogTitle>Selamat Datang, {name}</DialogTitle>
+      <DialogTitle>
+        <div
+          style={{
+            width: '100%',
+            position: 'relative',
+            cursor: 'pointer',
+            display: 'flex', // Ensures the div takes up the full space
+            justifyContent: 'center', // Centers the content
+            alignItems: 'center' // Centers the content
+          }}
+        >
+          <div
+            style={{
+              width: '256px',
+              height: '75px',
+              position: 'relative',
+              cursor: 'pointer',
+              display: 'flex', // Ensures the div takes up the full space
+              justifyContent: 'center', // Centers the content
+              alignItems: 'center' // Centers the content
+            }}
+          >
+            <Image
+              src='/images/logo_pesona.webp'
+              alt='Pesona Kahuripan'
+              fill
+              priority
+              style={{
+                objectFit: 'contain',
+              }}
+            />
+          </div>
+        </div>
+      </DialogTitle>
       <DialogContent>
-        <Grid container spacing={3} sx={{ mb: 3, p: 2 }}>
+        <Grid container spacing={3} sx={{mb: 3, p: 2}}>
           <Grid item xs={12}>
             <Box
               component='div'
